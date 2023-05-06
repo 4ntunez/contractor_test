f506798602b5a4ee3e62ccf876a91aca70fffc46
antunezcantero <ariel.antunezcantero@gmail.com>
2023-05-06 06:40:00 -0400

    Removed ngrok from CORS

--- contractor_test/settings.py
+++ contractor_test/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://ed2e-152-207-132-224.ngrok-free.app'
 )
 
 ROOT_URLCONF = 'contractor_test.urls'
