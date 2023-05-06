526ff2d983f50d10ad9495fa4f2e05c874eaba88
antunezcantero <ariel.antunezcantero@gmail.com>
2023-05-06 06:31:57 -0400

    updated cors

--- contractor_test/settings.py
+++ contractor_test/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://ed2e-152-207-132-224.ngrok-free.app'
 )
 
 ROOT_URLCONF = 'contractor_test.urls'
